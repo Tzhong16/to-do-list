ca42358bb5b805d2cc6584312daefec1cb14395b
Terence Zhong <t.zhong16@gmail.com>
2019-05-06 12:23:54 +1200

    fixed bug of create button

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,32 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Input extends Component {
-  render() {
-    return (
-      <div id="input " className="container ">
-        <form onSubmit={this.props.onSubmit}>
-          <div className="form-group  d-flex mt-3">
-            <input
-              value={this.props.note}
-              onChange={this.props.onChange}
-              type="text"
-              className="form-control p-2 w-100 m-auto"
-              placeholder="Input something to do..."
-            />
-            <button
-              className={
-                this.props.note === ''
-                  ? 'btn  btn-primary flex-shrink-1 ml-2 disabled'
-                  : 'btn  btn-primary flex-shrink-1 ml-2 '
-              }
-            >
-              Create
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+const Input = ({ onChange, onSubmit, note }) => {
+  return (
+    <div id="input " className="container ">
+      <form onSubmit={onSubmit}>
+        <div className="form-group  d-flex mt-3">
+          <input
+            value={note}
+            onChange={onChange}
+            type="text"
+            className="form-control p-2 w-100 m-auto"
+            placeholder="Input something to do..."
+          />
+          <button
+            className={'btn  btn-primary flex-shrink-1 ml-2'}
+            disabled={note === '' ? true : false}
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Input;
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import landing_img from '../img/landing_img.jpg';
 
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="https://terencezhong.com/">
-            Home Page
-          </a>
-        </nav>
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="https://terencezhong.com/">
+          Home Page
+        </a>
+      </nav>
 
-        <img src={landing_img} alt="landing" />
-      </div>
-    );
-  }
-}
+      <img src={landing_img} alt="landing" />
+    </div>
+  );
+};
 
 export default NavBar;
